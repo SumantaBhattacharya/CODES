7179ba4fac4058f8b2234fb17ffaf9ba1e306c76
SumantaBhattacharya <sumanta2004@gmail.com>
2026-07-06 11:05:50 +0530

    - *`Redux`* *is for client-side state (e.g., shopping cart items, dark mode)* - *`TanStack Query`* *is for server-state It’s designed to hold data fetched from an API (e.g., the list of products, a single product, user data from the backend like user profiles, comments).*

--- FullStack_P/API_E-Commerce/frontend/src/store/store.js
+++ FullStack_P/API_E-Commerce/frontend/src/store/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import cartReducer from "./cartSlice.js"
-import productReducer from "./productSlice.js"
+// import productReducer from "./productSlice.js"
 
 // The Store doesn't know what to do with that message until it passes it to the Reducer.
 // the Redux store itself doesn't work with an object of reducer functions for a single slice; it needs one single reducer function for that slice.
 const store = configureStore({
     reducer: {// The reducer is the logic engine that actually calculates the new state based on the action.
         cart: cartReducer,
-        product: productReducer,
+        // product: productReducer,
     }
 });
 
