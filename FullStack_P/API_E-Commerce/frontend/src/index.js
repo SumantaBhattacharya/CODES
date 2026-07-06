e886c5df7090fb5aa2a9e82892f5316f5e426b3b
SumantaBhattacharya <sumanta2004@gmail.com>
2026-07-06 11:04:52 +0530

    Add React Query client and provider to index.js for improved data fetching

--- FullStack_P/API_E-Commerce/frontend/src/index.js
+++ FullStack_P/API_E-Commerce/frontend/src/index.js
@@ -4,12 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import "./utils/i18.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Create a client
+const queryClient = new QueryClient(
+  {
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000, // 5 mins
+        refetchOnMount: false, // don't refetch on mount if data is fresh
+        refetchOnWindowFocus: false, // avoid refetch on tab switch
+        refetchOnReconnect: false, // avoid refetch when network reconnects what are these for
+      },
+    },
+  }
+)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App /> {/* App still contains Redux Provider*/}
+      {/* <ReactQueryDevtools initialIsOpen={false} />*/}
+      {/* By default, React Query Devtools are only included in bundles when process.env.NODE_ENV === 'development', so you don't need to worry about excluding them during a production build. */}
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
